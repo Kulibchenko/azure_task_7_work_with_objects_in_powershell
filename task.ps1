93b22297445cb40e2b639709fbe26351f30c5625
Kulib <kulibchenkovalera@gmail.com>
2024-10-23 20:05:16 +0300

    1st commit

--- task.ps1
+++ task.ps1
@@ -1,1 +1,9 @@
 # Write your code here
+$location = Get-ChildItem -Path./data
+$location_with_B2pts = @()
+foreach ($loc in $location) {
+    if (Get-Content $loc | ConvertFrom-Json | Where-Object {$_.name -eq "Standard_B2pts_v2"}) {
+        $location_with_B2pts += $loc.BaseName
+    }
+}
+$location_with_B2pts | ConvertTo-Json | Out-File "$PWD\result.json"
